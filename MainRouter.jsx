fb205d88566c4d48e67a37967cb491b13b8f03b8
Nooom01 <Nooom01@users.noreply.github.com>
2025-11-22 10:00:38 -0500

    Fix MainRouter - remove unused admin components

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,57 +1,38 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './src/components/Home'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './src/home'
 import About from './src/about'
 import Contact from './src/contact'
 import Education from './src/education'
 import Project from './src/project'
 import Services from './src/services'
-import Layout from './src/components/Layout'
-import SignIn from './src/signin'
 import SignUp from './src/signup'
+import SignIn from './src/signin'
+import AdminRoute from './src/admin/AdminRoute'
 import AdminDashboard from './src/admin/Dashboard'
-import ManageContacts from './src/admin/ManageContacts'
-import ManageProjects from './src/admin/ManageProjects'
-import ManageQualifications from './src/admin/ManageQualifications'
-import PrivateRoute from './src/components/PrivateRoute'
-import AdminRoute from './src/components/AdminRoute'
+import ManageUsers from './src/admin/ManageUsers'
 
-const MainRouter = () => {
+export default function MainRouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="education" element={<Education />} />
-        <Route path="project" element={<Project />} />
+        <Route path="projects" element={<Project />} />
         <Route path="services" element={<Services />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="signin" element={<SignIn />} />
         <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
         
         <Route path="admin" element={
           <AdminRoute>
             <AdminDashboard />
           </AdminRoute>
-        } />
-        <Route path="admin/contacts" element={
-          <AdminRoute>
-            <ManageContacts />
-          </AdminRoute>
-        } />
-        <Route path="admin/projects" element={
-          <AdminRoute>
-            <ManageProjects />
-          </AdminRoute>
-        } />
-        <Route path="admin/qualifications" element={
-          <AdminRoute>
-            <ManageQualifications />
-          </AdminRoute>
-        } />
+        }>
+          <Route path="users" element={<ManageUsers />} />
+        </Route>
       </Route>
     </Routes>
   )
 }
-
-export default MainRouter
